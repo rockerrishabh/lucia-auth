134145d3fd49070ec400199b9880b05e8fa11b55
Rishabh Kumar <rishabh.6542@gmail.com>
2024-07-25 16:34:00 +0530

    fifth commit

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,4 @@
-import "@/drizzle/envConfig";
+import "@/db/envConfig";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
 import * as schema from "./schema";
